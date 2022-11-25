6531628e2aa610ffa5e479e504e07acd18edee58
Alessandro Pedroso <alessandropedrosoti@gmail.com>
2022-11-25 16:08:11 -0300

    Validando campos com mensagens

--- pos-java-maven-hibernate/src/main/java/manegedBean/UsuarioPessoaManagedBean.java
+++ pos-java-maven-hibernate/src/main/java/manegedBean/UsuarioPessoaManagedBean.java
@@ -3,8 +3,10 @@ package manegedBean;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import Model.UsuarioPessoa;
 import dao.DaoGeneric;
@@ -14,7 +16,7 @@ import dao.DaoGeneric;
 public class UsuarioPessoaManagedBean {
 	
 	private UsuarioPessoa usuarioPessoa = new UsuarioPessoa();
-	private DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<>();
+	private DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
 	private List<UsuarioPessoa> listUsuarioPessoa = new ArrayList<UsuarioPessoa>();
 	
 	
@@ -30,6 +32,7 @@ public class UsuarioPessoaManagedBean {
 	public String salvar() {
 		
 		daoGeneric.salvar(usuarioPessoa);
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Informação: ", "Salvo com sucesso!"));
 		
 		return "";
 	}
@@ -39,6 +42,15 @@ public class UsuarioPessoaManagedBean {
 		usuarioPessoa = new UsuarioPessoa();
 		
 		return"";
+	}
+	
+	public String remover() {
+		
+		daoGeneric.deletarPoId(usuarioPessoa);
+		usuarioPessoa = new UsuarioPessoa();
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Informação: ", "Removido com sucesso!"));
+		
+		return "";
 	}
 	
 	public List<UsuarioPessoa> getListUsuarioPessoa() {
